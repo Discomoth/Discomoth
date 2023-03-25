111e9532ff1e3dee5c7fbbc17c0834bf7c214b25
Discomoth <raineyratcliff@gmail.com>
2023-03-24 23:24:27 -0700

    Initial commit

---
+++ timeExperiments/counterSerial/counterSerial.py
@@ -0,0 +1,27 @@
+import serial
+import time
+import os
+import sys
+
+
+counter = serial.Serial('/dev/ttyUSB0')
+counter.baud = 9600
+counter.timeout = 2
+
+outputLocation = os.path.dirname(os.path.abspath(__file__))
+print(outputLocation)
+
+with open(outputLocation + '/output.txt', 'w'):
+	pass
+
+while True:
+
+	if counter.in_waiting == 0:
+		pass
+	else:
+		readValue = counter.readline().decode()
+
+		with open(outputLocation + '/output.txt', 'a') as outputFile:
+			cleanedData = str((readValue)).replace('FA', '')
+			outputFile.write(str((readValue)).replace('FA', ''))
+			print(cleanedData)
